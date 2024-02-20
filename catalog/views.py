426205350f1104eff4d89e7dd0b03eb9cd1519eb
Samiksha <mailtosamiksha.s@gmail.com>
2024-02-20 05:08:20 +0000

    added generic html template

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book,Book_Copy_Info,Language,Genre,Author
+
+def index(request):
+    total_num_books = Book.objects.count()
+    total_num_Book_Copy_Info = Book_Copy_Info.objects.count()
+    total_num_Book_Copy_Info_avail = Book_Copy_Info.objects.filter(status_exact = 'a').count()
+    total_num_authors = Author.objects.count()
+
+    context = {
+        'total_num_books' : total_num_books,
+        'total_num_Book_Copy_Info' : total_num_Book_Copy_Info,
+        'total_num_Book_Copy_Info_avail' : total_num_Book_Copy_Info_avail,
+        'total_num_authors' : total_num_authors,
+        'total_num_authors' : total_num_authors,
+
+    }
+
+    return render(request, 'indes,html', context = context)
--- shiksha/urls.py
+++ shiksha/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += ['catalog/',include('catalog.urls'),]
+
+
